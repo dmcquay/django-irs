158778919e33d6b8b5446857041ed85634865bb7
Dustin McQuay <dmcquay@gmail.com>
2010-02-27 00:48:01 -0700

    Modify ImageURL class to handle all potential combination of parameters correctly.

--- irs/util.py
+++ irs/util.py
@@ -2,8 +2,18 @@ class ImageURL:
     transformations = []
     def __init__(self, image_path_rel):
         self.image_path_rel = image_path_rel
-    def resize(self, width=0, height=0):
-        self.transformations.append('resize+w%d+h%d' % (width, height))
+    def resize(self, width=None, height=None, small=None):
+        if (small == None and width == None and height == None):
+            raise Exception, 'At least one of small, width, height must be specified'
+        if small == None:
+            if width == None:
+                self.transformations.append('resize+w%d' % (width))
+            elif height == None:
+                self.transformations.append('resize+h%d' % (height))
+            else:
+                self.transformations.append('resize+w%d+h%d' % (width, height))
+        else:
+                self.transformations.append('resize+s%d' % (small))
         return self
     def square_center_crop(self, size=0):
         self.transformations.append('sccrop+s%d' % (size))
