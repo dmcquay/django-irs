ac8d13a3482f2c0011f13574f27fbe9fd03c4bb8
Dustin McQuay <dmcquay@gmail.com>
2010-03-06 23:20:43 -0700

    Remove some applications and middleware that are unnecessary for the demo.

--- demo/lib/irsdemo/settings.py
+++ demo/lib/irsdemo/settings.py
@@ -58,9 +58,9 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.middleware.common.CommonMiddleware',
+    #'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
 ROOT_URLCONF = 'irsdemo.urls'
@@ -75,9 +75,9 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.sites',
     'irsdemo.photos',
 )
